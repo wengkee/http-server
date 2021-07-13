2e4bdfc4e416c981777d5f26849db4e2ebfa76d7
Weng Kee Teh <wengkee.teh@gmail.com>
2021-07-14 00:00:06 +0800

    - adding help message and arguments parser - changing default port and address - fix exception handling

--- http-server.py
+++ http-server.py
@@ -1,29 +1,51 @@
-import sys
+import argparse
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-PORT = 8888
-ADDRESS = ""
+
+def parsing_args():
+    parser = argparse.ArgumentParser(description="Host a HTTP/HTTPS server")
+    parser.add_argument("filename", metavar="filename", type=str,
+                        help="the filename of the data file to be hosted by the server")
+    parser.add_argument("-p, --port", type=int, default=8080, dest="port",
+                        help="the port of the server")
+    parser.add_argument("-a, --address", type=str, default="127.0.0.1", dest="address",
+                        help="the address of the server")
+    return parser.parse_args()
 
 
-class LauncherService(BaseHTTPRequestHandler):
+def check_file(file):
+    try:
+        with open(file, 'rb') as f:
+            return f.read()
+    except IOError:
+        return None
+
+
+class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        try:
-            with open(sys.argv[1], 'rb') as f:
-                self.send_response(200)
-                self.send_header('Content-type', 'text/plain')
-                self.end_headers()
-                self.wfile.write(f.read())
-            return
-        except IOError:
-            self.send_error(404, 'file not found')
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(content)
 
 
-try:
-    httpd = HTTPServer((ADDRESS, PORT), LauncherService)
-    print(f'http server is running at http://{httpd.server_address[0]}:{httpd.server_port}')
-    httpd.serve_forever()
-except:
-    httpd.shutdown()
+args = parsing_args()
+filename = args.filename
+address = args.address
+port = args.port
+content = check_file(filename)
 
+if content:
+
+    httpd = HTTPServer((address, port), Handler)
+    try:
+        print(f'http server is running at http://{address}:{port}')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Shutting down..")
+        httpd.shutdown()
+
+else:
+    print(f"The file named '{args.filename}' does not exists, please create it.")
 
--- readme.md
+++ readme.md
@@ -4,23 +4,47 @@ Simple HTTP server written in Python 3 which will read a file from and serve it 
 
 ## Prequisite
 * Install Python 3 
-* Prepare a content file, e.g. any-file.txt
+* Prepare a content file
 * (optional) Install `keytool` for generating PEM file for hosting with HTTPS
 
 ## Quickstart
 ### HTTP 
-1. Run the following to host on http://localhost:8888
-    ```
-    python http-server.py any-file.txt
-    ```
+```
+$ python http-server.py -h
+usage: http-server.py [-h] [-p, --port port] [-a, --address address] filename
+
+Host a HTTP/HTTPS server
+
+positional arguments:
+  filename              the filename of the data file to be hosted by the
+                        server
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p, --port port       the port of the server
+  -a, --address address
+                        the address of the server
+```
+
+Run the following to host on http://127.0.0.1:8080
+
+ ```
+ $ python http-server.py hello-world.txt
+ ```
+
+Check it out
+```
+$ curl -s http://127.0.0.1:8080
+Hello World
+```
 
 ### HTTPS
 1. Generate a PEM file
-    ```
-   chmod u+x keystore/gen_pem.sh && ./keystore/gen_pem.sh
-   ```
+ ```
+$ chmod u+x keystore/gen_pem.sh && ./keystore/gen_pem.sh
+```
 
 2. Run the following to host on https://localhost:8888
-    ```
-    python https-server.py any-file.txt
-    ```
+ ```
+$ python https-server.py any-file.txt
+ ```
